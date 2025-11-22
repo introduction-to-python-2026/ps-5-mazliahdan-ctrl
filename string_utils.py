f85d26b6d58b04e599bed0f4d33065c414179ecb
mazliahdan-ctrl <mazliahdan@gmail.com>
2025-11-22 13:45:21 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -63,17 +63,3 @@ def count_atoms_in_molecule(molecular_formula):
 
 
 
-def parse_chemical_reaction(reaction_equation):
-    """Takes a reaction equation (string) and returns reactants and products as lists.  
-    Example: 'H2 + O2 -> H2O' → (['H2', 'O2'], ['H2O'])"""
-    reaction_equation = reaction_equation.replace(" ", "")  # Remove spaces for easier parsing
-    reactants, products = reaction_equation.split("->")
-    return reactants.split("+"), products.split("+")
-
-def count_atoms_in_reaction(molecules_list):
-    """Takes a list of molecular formulas and returns a list of atom count dictionaries.  
-    Example: ['H2', 'O2'] → [{'H': 2}, {'O': 2}]"""
-    molecules_atoms_count = []
-    for molecule in molecules_list:
-        molecules_atoms_count.append(count_atoms_in_molecule(molecule))
-    return molecules_atoms_count
