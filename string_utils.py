15f75048444213e54226c666706e81c9ef32632a
mazliahdan-ctrl <mazliahdan@gmail.com>
2025-11-22 13:14:29 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,7 +1,8 @@
 
 
 
-def split_before_uppercases(formula):
+
+def split_before_each_uppercases(formula):
     parts = []
     current = ""
 
@@ -18,9 +19,12 @@ def split_before_uppercases(formula):
         parts.append(current)
 
     return parts
+ 
 
-def split_at_digit(formula):
-     digit_location = 1
+
+def split_at_first_digit(formula):
+    
+    digit_location = 1
 
     
     for ch in formula[1:]:
@@ -36,19 +40,26 @@ def split_at_digit(formula):
     number = int(formula[digit_location:])
     return prefix, number
 
+    
+
+
+
 
 def count_atoms_in_molecule(molecular_formula):
-    """Takes a molecular formula (string) and returns a dictionary of atom counts.  
-    Example: 'H2O' → {'H': 2, 'O': 1}"""
+    """Takes a molecular formula (string) and returns a dictionary of atom counts."""
 
-    # Step 1: Initialize an empty dictionary to store atom counts
+    parts = split_before_each_uppercases(molecular_formula)
+    atom_counts = {}
 
-    for atom in split_by_capitals(molecular_formula):
-        atom_name, atom_count = split_at_number(atom)
-        
-        # Step 2: Update the dictionary with the atom name and count
+    for part in parts:
+        element, count = split_at_first_digit(part)
 
-    # Step 3: Return the completed dictionary
+        if element not in atom_counts:
+            atom_counts[element] = 0
+
+        atom_counts[element] += count
+
+    return atom_counts
 
 
 
